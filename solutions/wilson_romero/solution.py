6990d468ae84b6ad2ff8f024fda6629fa748fd43
wilson-romero <me@wilsonromero.com>
2020-05-22 13:17:32 -0500

    [correction] challenge python 9

--- solutions/wilson_romero/solution.py
+++ solutions/wilson_romero/solution.py
@@ -1,13 +1,18 @@
+""" Module solution """
 from typing import List
 
-
 class Solution:
-
+    """ Challenge solution """
     def duplicate_zeros(self, arr: List[int]):
-        # AQUÍ VA TU SOLUCIÓN
-        indices = list(filter(lambda x: arr[x] == 0, range(len(arr))))
-        shift = 0
-        for i in indices:
-            arr.insert(i + shift, 0)
-            arr.pop()
-            shift = shift + 1
+        """ Modify the list by doubling the zeros and correcting the items to the right. """ 
+        next_index = 0
+        length = len(arr)
+        while True:
+            try:
+                next_index = arr.index(0, next_index)
+                arr.insert(next_index + 1, 0)
+                arr.pop()
+                next_index = next_index + 2
+            except ValueError:
+                break
+        arr = arr[:length]
